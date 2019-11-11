9df890360db09e2b83494f1aaa14f2bd1f535f13
Jordan Christensen <jordan.c4922@gmail.com>
2019-11-12 01:07:13 +0800

    Add initial Xcode proj

---
+++ Contacts-Hybrid/Contacts-Hybrid/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Contacts-Hybrid
+//
+//  Created by Jordan Christensen on 11/12/19.
+//  Copyright © 2019 Mazjap Co. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
